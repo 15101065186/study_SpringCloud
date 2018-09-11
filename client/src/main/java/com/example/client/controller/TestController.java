ca06eadaa9a09e594a2d101bc7f83a5005d41944
libo <libo@sudiyi.cn>
2018-09-11 17:44:46 +0800

    配置中心

--- client/src/main/java/com/example/client/controller/TestController.java
+++ client/src/main/java/com/example/client/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RefreshScope
+@Component
 @RestController
 public class TestController {
 
